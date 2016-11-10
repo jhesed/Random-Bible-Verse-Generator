c8c4da772be6dbded406601cbe7e1f60a7e65d54
Jhesed Tacadena (Ben/TAS/Other <jhesed.tacadena@towerswatson.com>
2016-11-10 17:49:17 +0800

    Added `Credentials` as pop-up dialog

--- app/src/main/java/jsos/randomverse/MainActivity.java
+++ app/src/main/java/jsos/randomverse/MainActivity.java
@@ -3,9 +3,14 @@ package jsos.randomverse;
 import android.database.Cursor;
 import android.database.DatabaseUtils;
 import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -34,7 +39,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+//        MobileAds.initialize(getApplicationContext(), "ca-app-pub-3940256099942544~3347511713");
+//        AdView mAdView = (AdView) findViewById(R.id.adView);
+//        AdRequest adRequest = new AdRequest.Builder().build();
+//        mAdView.loadAd(adRequest);
+//    }
         BibleV1.generateQuery();
 
 //        dbHelper = new VerseDBHelper(this);
@@ -108,6 +117,51 @@ public class MainActivity extends AppCompatActivity {
 //                db.close();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void showAboutDialog() {
+        /*
+        * Shows dialog Box for adding note.
+        * TODO:
+        *   1. Update User interface
+        *   2. Add Icon
+        * */
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // Get the layout inflater
+        LayoutInflater inflater = this.getLayoutInflater();
+
+        // Inflate and set the layout for the dialog
+        // Pass null as the parent view because its going in the dialog layout
+        builder.setView(inflater.inflate(R.layout.dialog_menu_about, null));
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 
 
