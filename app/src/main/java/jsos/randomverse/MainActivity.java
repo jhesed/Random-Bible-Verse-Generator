a08ddd2e9cf72d57519406096f1a5b9385c11f43
Jhesed Tacadena (Ben/TAS/Other <jhesed.tacadena@towerswatson.com>
2016-11-10 18:32:05 +0800

    Replaced launcher with Bible logo

--- app/src/main/java/jsos/randomverse/MainActivity.java
+++ app/src/main/java/jsos/randomverse/MainActivity.java
@@ -135,10 +135,9 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-
+        if (id == R.id.menu_about) {
+            showAboutDialog();
+        }
         return super.onOptionsItemSelected(item);
     }
 
