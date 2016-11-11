7c0935254b4cac20eca51b90262eac9688e339d1
Jhesed Tacadena (Ben/TAS/Other <jhesed.tacadena@towerswatson.com>
2016-11-11 10:41:45 +0800

    Code cleanup

--- app/src/main/java/jsos/randomverse/MainActivity.java
+++ app/src/main/java/jsos/randomverse/MainActivity.java
@@ -1,8 +1,5 @@
 package jsos.randomverse;
 
-import android.database.Cursor;
-import android.database.DatabaseUtils;
-import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,8 +15,6 @@ import android.widget.TextView;
 import java.util.Random;
 
 import jsos.randomverse.bible.BibleV1;
-import jsos.randomverse.db.VerseContract;
-//import jsos.randomverse.db.VerseDBHelper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
     private TextView contentEngNIV;
     private TextView contentFilMBB;
 
-//    private VerseDBHelper dbHelper;
     private int lastVerseId = -1;
 
     public static final String TAG = "MainActivity";
@@ -45,8 +39,6 @@ public class MainActivity extends AppCompatActivity {
 //        mAdView.loadAd(adRequest);
 //    }
         BibleV1.generateQuery();
-
-//        dbHelper = new VerseDBHelper(this);
 
         // Sets Random Button
         btnRandom = (Button) findViewById(R.id.buttonRandom) ;
@@ -63,58 +55,24 @@ public class MainActivity extends AppCompatActivity {
                 contentEngNIV = (TextView) findViewById(R.id.contentEngNIV);
                 contentFilMBB = (TextView) findViewById(R.id.contentFilMBB);
 
-                // Count SQL Entries
-                // String query = "SELECT COUNT(*) FROM " + VerseContract.VerseEntry.TABLE + ";";
-                // Log.d(TAG, query);
-//                SQLiteDatabase db = dbHelper.getReadableDatabase();
-//                int max = VerseContract.VERSE_COUNT;
-//                 Cursor cursor = db.rawQuery(query, null);
-//                 int max = cursor.getCount();
-//                long max  = DatabaseUtils.queryNumEntries(
-//                     db, VerseContract.VerseEntry.TABLE);
-//                     db, VerseContract.DB_NAME);
-//                int max = 27;  // TEST HARDCODED!!!!
-//                Log.d(TAG, "Verse Count: " + max);
-
                 // Generate random number
                 // ---------- Try arraylist ------------
                 int max = BibleV1.versesQuery.size();
                 Random rand = new Random();
                 int index = rand.nextInt(max);
                 Log.d(TAG, "random number initial: " + index);
+
                 // Do not use previous verse
                 while (lastVerseId == index) {
                     index = rand.nextInt() + 1;
-                    Log.d(TAG, "random number: " + index);
                 }
                 lastVerseId = index;
 
-//                Log.d(TAG, "======== " + BibleV1.versesQuery.get(index).get(0));
                 titleHeader.setText(BibleV1.versesQuery.get(index).get(0).toString());
                 titleEngNIV.setText("NIV");
                 titleFilMBB.setText("MBB");
                 contentEngNIV.setText(BibleV1.versesQuery.get(index).get(1).toString());
                 contentFilMBB.setText(BibleV1.versesQuery.get(index).get(2).toString());
-
-
-                // Retrieve new content from database and update content
-//                String query = "SELECT * FROM " + VerseContract.VerseEntry.TABLE + " WHERE " +
-//                        VerseContract.VerseEntry._ID + " = "+ index + ";";
-//                Log.d(TAG, query);
-//                Cursor cursor = db.rawQuery(query, null);
-//                cursor.moveToNext();
-//                String header = cursor.getString(cursor.getColumnIndex(VerseContract.VerseEntry.COL_NAME));
-//                Log.d(TAG, "HEADER: " + header);
-
-//                titleHeader.setText(cursor.getString(cursor.getColumnIndex(VerseContract.VerseEntry.COL_NAME)));
-//                titleEngNIV.setText("NIV");
-//                titleFilMBB.setText("MBB");
-//                contentEngNIV.setText(cursor.getString(cursor.getColumnIndex(VerseContract.VerseEntry.COL_CONTENT_ENG_NIV)));
-//                contentFilMBB.setText(cursor.getString(cursor.getColumnIndex(VerseContract.VerseEntry.COL_CONTENT_FIL_MB)));
-
-                // Close database connection
-//                cursor.close();
-//                db.close();
             }
         });
     }
@@ -129,13 +87,11 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        /* Adds behaviors to menu */
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.menu_about) {
+            // Shows information dialog
             showAboutDialog();
         }
         return super.onOptionsItemSelected(item);
@@ -143,12 +99,9 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void showAboutDialog() {
-        /*
-        * Shows dialog Box for adding note.
-        * TODO:
-        *   1. Update User interface
-        *   2. Add Icon
-        * */
+        /**
+        Displays dialog box of developer information
+         */
 
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         // Get the layout inflater
@@ -160,7 +113,6 @@ public class MainActivity extends AppCompatActivity {
 
         AlertDialog dialog = builder.create();
         dialog.show();
-
     }
 
 
