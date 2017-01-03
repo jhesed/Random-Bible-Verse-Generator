8be830f60463bdce795e51cf4bdb98d631f21896
Jhesed Tacadena (Ben/TAS/Other <jhesed.tacadena@towerswatson.com>
2017-01-03 12:36:43 +0800

    updated target SDK to latest version (25)

--- app/src/main/java/jsos/randomverse/VerseDetailsActivity.java
+++ app/src/main/java/jsos/randomverse/VerseDetailsActivity.java
@@ -18,7 +18,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
