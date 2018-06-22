c4eddf19dead1b9e3c1f0ee26a25a6c558dc05cd
jhesed.tacadena <jhesed.tacadena@gmail.com>
2018-06-22 12:36:12 +0800

    v1.1: Implementation of working clickable verses with scrolling for complete verse list

--- app/src/main/java/com/jsos/randomverse2/adapters/VerseAdapter.java
+++ app/src/main/java/com/jsos/randomverse2/adapters/VerseAdapter.java
@@ -27,12 +27,10 @@ public class VerseAdapter extends ArrayAdapter<Verse> {
 
     private final String TAG = "verseListAdapter";
     private Context context;
-    private ArrayList<Verse> verses;
 
     public VerseAdapter(Context adapterContext, ArrayList<Verse> verseList) {
-        super(adapterContext, R.layout.activity_bible_memorization_verse_list, verseList);
+        super(adapterContext, R.layout.activity_verse_list, verseList);
         context = adapterContext;
-        verses = verseList;
     }
 
     @NonNull
@@ -43,41 +41,37 @@ public class VerseAdapter extends ArrayAdapter<Verse> {
          * */
 
         View view = convertView;
-        ViewHolder viewHolder;
-        if (view == null) {
-//            view = LayoutInflater.from(getContext()).inflate(R.layout.activity_bible_memorization_verse_list, null);
-            view = LayoutInflater.from(getContext()).inflate(R.layout.activity_verse_list, null);
-//            Verse verse = verses.get(position);
-            Verse verse = getItem(position);
-            final int verseId = verse.id;
+        final ViewHolder viewHolder;
+        Verse verse = getItem(position);
 
+        if (view == null) {
+            view = LayoutInflater.from(getContext()).inflate(R.layout.activity_verse_list, null);
             // Lookup view for data population
             viewHolder = new ViewHolder();
-            viewHolder.verseId = verse.id;
-            viewHolder.verseName = (TextView) view.findViewById(R.id.verseItem);
-
-            // Populate the data into the template view using the data object
-            viewHolder.verseName.setText(verse.name);
-
-            view.setTag(viewHolder);
-            view.setLongClickable(true);
-
-            /* SECTION : Events */
-
-            view.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(context, VerseDetailsActivity.class);
-                    intent.putExtra("verseId", verseId);
-                    context.startActivity(intent);
-                }
-            });
         }
         else {
             viewHolder = (ViewHolder)view.getTag();
         }
 
+        // Populate the data into the template view using the data object
+        viewHolder.verseId = verse.id;
+        viewHolder.verseName = (TextView) view.findViewById(R.id.verseItem);
+        viewHolder.verseName.setText(verse.name);
+
+        view.setTag(viewHolder);
+        view.setLongClickable(true);
+
+        /* SECTION : Events */
+
+        view.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, VerseDetailsActivity.class);
+                intent.putExtra("verseId", viewHolder.verseId);
+                context.startActivity(intent);
+            }
+        });
         return view;
     }
 
